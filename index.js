cb6eea8f9a6006173b0282527bdd97c01317c019
Ben McIntyre <ben@bens-mbp.attlocal.net>
2022-07-25 18:05:47 -0700

    Changed window prompt to window alert for ending

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ if (answer === "l") {
       "You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying(s), but you wonder if you should alert the world to this magical safe haven(a)."
     );
     if (answer === "s") {
-      answer = window.prompt(
+      answer = window.alert(
         "You live happily amongst the cats for the rest of your days."
       );
     } else if (answer === "a") {
-      answer = window.prompt(
+      answer = window.alert(
         "After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless."
       );
     }
@@ -21,11 +21,11 @@ if (answer === "l") {
       "You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase(s), and a much quicker, but rickety-looking ladder(l) that leads up toward the light. Which do you take?"
     );
     if (answer === "l") {
-      answer = window.prompt(
+      answer = window.alert(
         "After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home."
       );
     } else if (answer === "s") {
-      answer = window.prompt(
+      answer = window.alert(
         "After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever."
       );
     }
@@ -39,11 +39,11 @@ if (answer === "l") {
       "The dragon wakes up and sits upright. You only have a moment to respond, to stay(s) or run(r)?"
     );
     if (answer === "s") {
-      answer = window.prompt(
+      answer = window.alert(
         "You and the dragon have an uplifting conversation about local politics and become lifelong friends."
       );
     } else if (answer === "r") {
-      answer = window.prompt(
+      answer = window.alert(
         "Quickly, you run back to the cave's entrance. Sheepish, you return home."
       );
     }
@@ -52,11 +52,11 @@ if (answer === "l") {
       "After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it(d) or pick it(p). Which do you do?"
     );
     if (answer === "d") {
-      answer = window.prompt(
+      answer = window.alert(
         "You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch."
       );
     } else if (answer === "p") {
-      answer = window.prompt(
+      answer = window.alert(
         "You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust."
       );
     }
